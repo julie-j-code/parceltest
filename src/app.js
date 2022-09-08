72ad650208e9e81f1a463cb805cfafaff47263cb
Julie J <jeannet.julie@gmail.com>
2022-09-08 07:52:02 +0200

    Implementation calculator via app.js pour gestion des évènements de la nouvelle calculatrice - local ok test gh-pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import calculator from "./calculator";
 import { Calculs } from "./calculs";
 
+// première partie qui fonctionne enfin normalement après build et deploy sur gh-pages
 const calculs = new Calculs();
 btnAdd.addEventListener("click", () => {
     let res = calculs.add(Number(textA.value), Number(textB.value));
@@ -18,3 +19,29 @@ btnDiv.addEventListener("click", () => {
     let res = calculs.div(Number(textA.value), Number(textB.value));
     result.innerHTML = res;
 })
+
+// implementation deuxième partie : calculatrice ++ issue de mon calculator
+// pour la seconde calculatrice plus élaborée
+const btnSolve = document.querySelector("#r5-3");
+const btnClear = document.querySelector("#r1-1");
+const btnPerc = document.querySelector("#r1-3");
+
+btnClear.addEventListener("click", () => {
+    calculator.forclear();
+})
+btnSolve.addEventListener("click", () => {
+    calculator.solve();
+})
+btnPerc.addEventListener("click", () => {
+    calculator.perc();
+})
+
+const buttons = document.querySelectorAll(".calculator button");
+// console.log(buttons);
+
+buttons.forEach(btn => {
+    // console.log(btn.textContent);
+    btn.textContent != '=' && btn.textContent != 'C' && btn.textContent != '%' ? btn.addEventListener("click", () => calculator.display(btn.textContent)) : ""
+});
+
+// Et c'est tout pour la deuxième. Impeccable
